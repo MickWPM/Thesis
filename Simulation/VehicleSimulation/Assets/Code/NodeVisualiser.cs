ed138ecf739388de9d6f58ef6a834c70673e1a86
Mick M <mickwpmcdonnell@gmail.com>
2019-05-31 14:32:09 +1000

    Added helper functions to node visualiser for node gathering
    These need to be refactored later

--- Simulation/VehicleSimulation/Assets/Code/NodeVisualiser.cs
+++ Simulation/VehicleSimulation/Assets/Code/NodeVisualiser.cs
@@ -5,6 +5,14 @@ using UnityEditor;
 
 public class NodeVisualiser : MonoBehaviour
 {
+    public List<Vector3> nodes = new List<Vector3>();
+    public Transform fromPos;
+
+    private void Start()
+    {
+        nodes = GetNodesFromPos(fromPos.position);
+    }
+
     private void OnDrawGizmosSelected()
     {
         for (int i = 0; i < transform.childCount; i++)
@@ -18,5 +26,39 @@ public class NodeVisualiser : MonoBehaviour
             Gizmos.color = Color.black;
             Gizmos.DrawWireSphere(t.position, 2f);
         }
+    }
+
+    public List<Vector3> GetNodesFromPos(Vector3 currentPos)
+    {
+        List<Vector3> nodes = GetAllNodes();
+        float minDist = float.MaxValue;
+        int nodeIndex = -1;
+
+        for (int i = 0; i < nodes.Count; i++)
+        {
+            float thisDist = Vector3.Distance(currentPos, nodes[i]);
+            if (thisDist > minDist)
+                continue;
+
+            minDist = thisDist;
+            nodeIndex = i;
+        }
+
+        nodes.RemoveRange(0, nodeIndex);
+
+        return nodes;
+    }
+
+    public List<Vector3> GetAllNodes()
+    {
+        List<Vector3> nodes = new List<Vector3>(transform.childCount);
+        Transform[] childTransforms = GetComponentsInChildren<Transform>();
+
+        for (int i = 1; i < childTransforms.Length; i++)
+        {
+            nodes.Add(transform.GetChild(i-1).position);
+        }
+
+        return nodes;
     }
 }
