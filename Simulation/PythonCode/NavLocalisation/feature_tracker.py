9721f6f939956029f34b425452b6e3bc88796740
Mick M <mickwpmcdonnell@gmail.com>
2019-10-03 15:55:41 +1000

    Added Farneback flow and mask shifting to feature tracker

--- Simulation/PythonCode/NavLocalisation/feature_tracker.py
+++ Simulation/PythonCode/NavLocalisation/feature_tracker.py
@@ -40,19 +40,131 @@ def update_feature_masks(distance_from_feature, feature, mask_resolution, ipm_di
     """
     pass
 
-def check_feature(feature_mask, road_surface, probability_threshold=0.7):
+def shift_mask(mask, coord_shift):
+    #Shift of negative means point moving to the left so we have to move to the right
+    #So grab from 0 and move to coord
+    
+    #coord_shift = (0,100) #Down 100
+    #coord_shift = (0,-50) #SHOULD BE - Up 100 -- DOESNT WORK
+
+    #coord_shift = (100, 0) #LEFT 100
+    #coord_shift = (-100, 0) #SHOULD BE - Right 100 -- DOESNT WORK
+
+    #coord_shift = (coord_shift[1], coord_shift[0])
+    #x = -50
+    #y = 0
+    
+    if  np.absolute(coord_shift[0]) > 0 and  np.absolute(coord_shift[1]) > 0:
+        #Move X to the right
+        if coord_shift[0] < 0:
+            #Move X to the right and Y UP
+            if coord_shift[1] < 0:
+                #mask[0:coord_shift[1], :] = mask[-coord_shift[1]:, :]
+                #mask[coord_shift[1]:, :] = 0
+                #mask[:, -coord_shift[0]:] = mask[:, 0:coord_shift[0]]
+                #mask[:, :-coord_shift[0]] = 0
+                
+                mask[0:coord_shift[1], -coord_shift[0]:] = mask[-coord_shift[1]:, 0:coord_shift[0]]
+                mask[coord_shift[1]:, :-coord_shift[0]] = 0
+                
+
+            #Move X to the right and Y Down
+            elif coord_shift[1] > 0:
+                #mask[coord_shift[1]:, :] = mask[:-coord_shift[1], :]
+                #mask[0:coord_shift[1], :] = 0
+                #mask[:, -coord_shift[0]:] = mask[:, 0:coord_shift[0]]
+                #mask[:, :-coord_shift[0]] = 0
+                
+                mask[coord_shift[1]:, -coord_shift[0]:] = mask[:-coord_shift[1], 0:coord_shift[0]]
+                mask[0:coord_shift[1], :-coord_shift[0]] = 0
+                
+
+
+        #Move X to the left
+        elif coord_shift[0] > 0:
+            #Move X to the left and y UP
+            if coord_shift[1] < 0:
+                #mask[0:coord_shift[1], :] = mask[-coord_shift[1]:, :]
+                #mask[coord_shift[1]:, :] = 0
+                #mask[:, 0:-coord_shift[0]] = mask[:, coord_shift[0]:]
+                #mask[:, -coord_shift[0]:] = 0
+                
+                mask[0:coord_shift[1], 0:-coord_shift[0]] = mask[-coord_shift[1]:, coord_shift[0]:]
+                mask[coord_shift[1]:, -coord_shift[0]:] = 0
+                
+
+            #Move X to the left and y DOWN
+            elif coord_shift[1] > 0:
+                #mask[coord_shift[1]:, :] = mask[:-coord_shift[1], :]
+                #mask[0:coord_shift[1], :] = 0
+                #mask[:, 0:-coord_shift[0]] = mask[:, coord_shift[0]:]
+                #mask[:, -coord_shift[0]:] = 0
+                
+                mask[coord_shift[1]:, 0:-coord_shift[0]] = mask[:-coord_shift[1], coord_shift[0]:]
+                mask[0:coord_shift[1], -coord_shift[0]:] = 0
+                
+
+
+    elif np.absolute(coord_shift[0]) > 0:
+        #At this point, Y is zero, so just move X
+        #Move X RIGHT
+        if coord_shift[0] < 0:
+            mask[:, -coord_shift[0]:] = mask[:, 0:coord_shift[0]]
+            mask[:, :-coord_shift[0]] = 0
+        #Move X LEFT
+        else:
+            mask[:, 0:-coord_shift[0]] = mask[:, coord_shift[0]:]
+            mask[:, -coord_shift[0]:] = 0
+
+
+    elif np.absolute(coord_shift[1]) > 0:
+        #At this point, X is zero, so just move Y
+        #Move UP
+        if coord_shift[1] < 0:
+            mask[0:coord_shift[1], :] = mask[-coord_shift[1]:, :]
+            mask[coord_shift[1]:, :] = 0
+
+        #Move DOWN
+        elif coord_shift[1] > 0:    
+            mask[coord_shift[1]:, :] = mask[:-coord_shift[1], :]
+            mask[0:coord_shift[1], :] = 0
+
+    return mask
+
+def check_feature(feature_mask, road_surface, coord_shift=(0,0), probability_threshold=0.7):
     """
     Considers features elementwise
     """
+    coord_shift = (int(coord_shift[0]), int(coord_shift[1]))
     mask_probabilities = []
     masked_image = np.zeros(road_surface.shape, dtype=np.uint8)
+    shifted = np.absolute(coord_shift[0]) > 0 or  np.absolute(coord_shift[1]) > 0
+    
+    #TMP
+    #mask_unshifted = np.zeros(road_surface.shape, dtype=np.uint8)
+
     for mask in feature_mask:
         #TODO: GET RID OF THIS threshold??
         _, mask = cv2.threshold(mask, 200, 255, cv2.THRESH_BINARY)
+        
+        #TMP
+        #mask_unshifted = np.add(mask_unshifted, mask )
+
+        if shifted:
+            mask = shift_mask(mask, coord_shift)
+
         _, _, feature_masked_image = mask_image(road_surface, mask)
         masked_image = np.add(masked_image, feature_masked_image )
         mask_probabilities.append(np.sum(feature_masked_image ) / np.sum(mask))
     #masked_image for visualisation only
+    
+    #Visualise movement of mask
+    #if shifted:
+    #    cv2.imshow("Pre shift", mask_unshifted)
+    #    cv2.waitKey(0)
+    #    cv2.imshow("Post shift", masked_image)
+    #    cv2.waitKey(0)
+
     min_probability = np.amin(mask_probabilities)
     detected = min_probability>probability_threshold
     return detected,  mask_probabilities
@@ -80,6 +192,29 @@ def CheckMasksProbability(image, masks):
         masked_image = np.add(masked_image, feature_masked_image )
         mask_probabilities.append(np.sum(feature_masked_image ) / np.sum(mask))
     return mask_probabilities, masked_image
+
+
+def GetUpdatedRoadFeatureLocationFarneback(prev_im, cur_im, feature_coord, flow=None):
+    pyr_scale=0.5
+    levels	= 3
+    winsize = 15
+    iterations	= 3
+    poly_n	= 5
+    poly_sigma = 1.2
+    flags = 0
+    flow = cv2.calcOpticalFlowFarneback(prev_im, cur_im, flow, 0.5, 3, winsize, iterations, poly_n, poly_sigma, flags )
+
+    #ave_x = np.average(flow[:, :, 0], weights=(np.absolute(flow[:, :, 0]) > 0.2))
+    #ave_y = np.average(flow[:, :, 1], weights=(np.absolute(flow[:, :, 0]) > 0.2))
+
+    ave_x = np.mean(flow[400:500, 240:276, 0])
+    ave_y = np.mean(flow[400:500, 240:276, 1])
+    #ave_x = np.mean(flow[240:276, 400:500, 0])
+    #ave_y = np.mean(flow[240:276, 400:500, 1])
+
+    feature_coord = (feature_coord[0]+ave_x, feature_coord[1]+ave_y)
+    optical_features = None
+    return feature_coord, optical_features
 
 
 #https://github.com/opencv/opencv/blob/master/samples/python/lk_track.py
